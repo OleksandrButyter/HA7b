0afc923c712d1eadde6740df8cd6abb1e8a5e629
Oleksandr <olekbut@gmail.com>
2016-06-01 13:15:49 +0000

    fjsdbfj

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// JavaScript File
+function getFibonacci() {
+    var num = document.equation.a.value,
+        a = 1,
+        b = 1,
+        c, i;
+    if (num >= 3) {
+        for (i = 3; i <= num; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+    else {
+        c = a;
+    }
+    res.innerHTML = c;
+    return c;
+}
+
+var capturing = true;
+
+function btnClicked(e) {
+    var n = (prompt("Enter a number"));
+}
+
+btn.addEventListener('click', btnClicked, capturing);
+
+
+
